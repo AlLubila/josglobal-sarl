8218162b52754ce5a3bcb965bc399ae58cab5317
Albi Lubila <127430514+AlLubila@users.noreply.github.com>
2024-10-15 22:24:14 +0200

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,8 +6,7 @@ import { loginAdmin } from '@/utils/auth';
 import '@/styles/globals.css'
 import Cookies from 'js-cookie'; 
 
-
-// Function return a tailwind component for login page
+// Fonction qui retourne un composant Tailwind pour la page de connexion
 export default function AdminLogin() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -15,20 +14,20 @@ export default function AdminLogin() {
   const router = useRouter();
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log('Attempting login with:', email, password);
+    console.log('Tentative de connexion avec:', email, password);
     try {
       const isLoggedIn = await loginAdmin(email, password);
       if (isLoggedIn) {
-        console.log('Login successful');
+        console.log('Connexion réussie');
         Cookies.set("admin_token", "true", { expires: 1 });
         router.push("/admin/dashboard");
       } else {
-        console.log('Login failed');
-        setError("Invalid email or password");
+        console.log('Échec de la connexion');
+        setError("Email ou mot de passe incorrect");
       }
     } catch (error) {
-      console.error('Login error:', error);
-      setError("An error occurred during login. Please try again.");
+      console.error('Erreur lors de la connexion:', error);
+      setError("Une erreur est survenue lors de la connexion. Veuillez réessayer.");
     }
   };
 
@@ -38,7 +37,7 @@ export default function AdminLogin() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Connectez-vous à votre compte
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
@@ -46,7 +45,7 @@ export default function AdminLogin() {
                   htmlFor="email"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Your email
+                  Votre email
                 </label>
                 <input
                   type="email"
@@ -55,7 +54,7 @@ export default function AdminLogin() {
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                   className="outline-none bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="nom@entreprise.com"
                   required
                 />
               </div>
@@ -64,7 +63,7 @@ export default function AdminLogin() {
                   htmlFor="password"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
-                  Password
+                  Mot de passe
                 </label>
                 <input
                   type="password"
@@ -82,7 +81,7 @@ export default function AdminLogin() {
                 type="submit"
                 className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Login
+                Connexion
               </button>
             </form>
           </div>
