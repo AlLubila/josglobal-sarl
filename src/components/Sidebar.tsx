3939cd213262eece7e45bfb86cb49182ac7eec4e
Albi Lubila <127430514+AlLubila@users.noreply.github.com>
2024-10-15 22:51:55 +0200

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,37 +6,37 @@ import { FaNewspaper, FaUsers, FaStar, FaCogs, FaChartLine, FaShieldAlt } from '
 export default function Sidebar() {
   return (
     <aside className="bg-gray-800 text-white w-64 hidden sm:flex flex-col h-screen p-6 shadow-lg">
-      <h2 className="text-2xl font-bold mb-8 text-center">Admin Panel</h2>
+      <h2 className="text-2xl font-bold mb-8 text-center">Panneau d'administration</h2> {/* Admin Panel */}
       <nav>
         <ul className="space-y-6">
           <li>
             <Link href="/admin/dashboard/content-management" className="flex items-center text-lg hover:text-blue-500">
-              <FaNewspaper className="mr-3" /> Blog Manager
+              <FaNewspaper className="mr-3" /> Gestion des articles {/* Blog Manager */}
             </Link>
           </li>
           <li>
             <Link href="/admin/dashboard/user-management" className="flex items-center text-lg hover:text-blue-500">
-              <FaUsers className="mr-3" /> User Management
+              <FaUsers className="mr-3" /> Gestion des utilisateurs {/* User Management */}
             </Link>
           </li>
           <li>
             <Link href="/admin/dashboard/testimonials" className="flex items-center text-lg hover:text-blue-500">
-              <FaStar className="mr-3" /> Testimonials
+              <FaStar className="mr-3" /> Témoignages {/* Testimonials */}
             </Link>
           </li>
           <li>
             <Link href="/admin/dashboard/settings" className="flex items-center text-lg hover:text-blue-500">
-              <FaCogs className="mr-3" /> Settings
+              <FaCogs className="mr-3" /> Paramètres {/* Settings */}
             </Link>
           </li>
           <li>
             <Link href="/admin/dashboard/analystics" className="flex items-center text-lg hover:text-blue-500">
-              <FaChartLine className="mr-3" /> Analytics
+              <FaChartLine className="mr-3" /> Analytique {/* Analytics */}
             </Link>
           </li>
           <li>
             <Link href="/admin/dashboard/privacy-legal" className="flex items-center text-lg hover:text-blue-500">
-              <FaShieldAlt className="mr-3" /> Privacy & Legal
+              <FaShieldAlt className="mr-3" /> Confidentialité & Légal {/* Privacy & Legal */}
             </Link>
           </li>
         </ul>
