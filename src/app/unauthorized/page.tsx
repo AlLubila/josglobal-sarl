dddd824c76e211dc6e61b666583cf88da9fd5234
Albi Lubila <127430514+AlLubila@users.noreply.github.com>
2024-10-15 22:36:40 +0200

    Update page.tsx

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -16,20 +16,20 @@ export default function Unauthorized() {
       <main className="flex flex-col items-center justify-center flex-grow px-4 text-center">
         <h2 className="text-4xl font-bold mb-4">403 - Unauthorized</h2>
         <p className="text-lg mb-6">
-          You do not have permission to view this page. Please contact the administrator if you think this is a mistake.
+          Vous n'êtes pas autorisé à voir cette page. Veuillez contacter l'administrateur si vous pensez qu'il s'agit d'une erreur.
         </p>
         <button
           onClick={() => router.push('/')}
           className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
         >
-          Go to Home
+          Retourner à la page d'accueil
         </button>
       </main>
 
       {/* Footer */}
       <footer className="py-4 bg-gray-900 text-center">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Josglobal Sarl. All rights reserved.
+          &copy; {new Date().getFullYear()} Josglobal Sarl. Tous droits réservés.
         </p>
       </footer>
     </div>
