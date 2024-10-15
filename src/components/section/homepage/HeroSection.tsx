c9041ae87834db3eb98dd281043c0850e5f0e682
Albi Lubila <127430514+AlLubila@users.noreply.github.com>
2024-10-15 22:42:33 +0200

    Update HeroSection.tsx

--- src/components/section/homepage/HeroSection.tsx
+++ src/components/section/homepage/HeroSection.tsx
@@ -1,60 +1,61 @@
 'use client'
 
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 import { FaPhone } from "react-icons/fa";
 import { IoIosInformationCircle } from "react-icons/io";
 
-import styles from '@/styles/section/homepage/heroSection.module.css'
+import styles from '@/styles/section/homepage/heroSection.module.css';
 import { useRouter } from 'next/navigation';
 
 export default function HeroSection() {
-  const router = useRouter()
+  const router = useRouter();
 
   const toContact = () => {
-    router.push('/contact')
+    router.push('/contact');
   }
   const toAbout = () => {
-    router.push('/about')
+    router.push('/about');
   }
+  
   return (
     <div>
-        <section className={styles.heroSection}>
-          <div className={styles.leftPart}>
-            <p>
-              <span className={styles.title}>JOSGLOBAL Sarl</span> connects
-              businesses in Central Africa with top suppliers. Based in
-              Kinshasa, we offer high-quality B2B solutions for diverse needs.
-            </p>
-            <p>Streamline your sourcing and expand your reach with us.</p>
-            <p>Get in touch or learn more today!</p>
-            <div className={styles.buttons}>
-              <button onClick={toContact}>
-                Get in Touch{" "}
-                <span>
-                  <FaPhone />
-                </span>
-              </button>
-              <button onClick={toAbout}>
-                Learn More{" "}
-                <span>
-                  <IoIosInformationCircle size="30px" />
-                </span>
-              </button>
-            </div>
+      <section className={styles.heroSection}>
+        <div className={styles.leftPart}>
+          <p>
+            <span className={styles.title}>JOSGLOBAL Sarl</span> connecte
+            les entreprises en Afrique centrale avec des fournisseurs de premier plan. Basés à
+            Kinshasa, nous offrons des solutions B2B de haute qualité pour divers besoins.
+          </p>
+          <p>Rationalisez votre approvisionnement et élargissez votre portée avec nous.</p>
+          <p>Contactez-nous ou apprenez-en plus dès aujourd'hui !</p>
+          <div className={styles.buttons}>
+            <button onClick={toContact}>
+              Contactez-nous{" "}
+              <span>
+                <FaPhone />
+              </span>
+            </button>
+            <button onClick={toAbout}>
+              En savoir plus{" "}
+              <span>
+                <IoIosInformationCircle size="30px" />
+              </span>
+            </button>
           </div>
+        </div>
 
-          <div className={styles.rightPart}>
-            <Image
-              src="/businessNetworking-Photoroom.png"
-              alt="Hero section image"
-              width={500}
-              height={500}
-              className={styles.img}
-              priority
-            />
-          </div>
-        </section>
+        <div className={styles.rightPart}>
+          <Image
+            src="/businessNetworking-Photoroom.png"
+            alt="Image de la section héros"
+            width={500}
+            height={500}
+            className={styles.img}
+            priority
+          />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
